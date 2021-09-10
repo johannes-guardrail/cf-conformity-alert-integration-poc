d6d5eb51c2ea8314873948efb2a8eb95e9768aad
johannes-guardrail <johannes@guardrail.me>
2021-09-10 17:06:17 -0300

    add variable as account id reference

--- terraform-aws/sns.tf
+++ terraform-aws/sns.tf
@@ -2,16 +2,14 @@ variable "sns_arn" {
     type = string
 }
 resource "conformity_communication_setting" "sns_setting" {
-    
     sns {
         # get the ARN from the SNS created by cloudformation
         arn          = var.sns_arn
         channel_name = "conformity-sns"
     }
-
     relationships {
         account {
-            id = "bbdc1bd1-78cb-40b7-839a-cf4a37efbf1c"
+            id = conformity_aws_account.aws.id
         }
     }
 }
